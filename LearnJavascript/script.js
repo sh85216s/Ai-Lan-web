a1abfe4acf313ad8417786c859a1386800e9f3d7
shiarng <88964278+sh85216s@users.noreply.github.com>
2024-01-17 19:00:33 +0800

    113.01.17
    新增
    學習Javascript、自動跳轉到酷英-英聽王

--- LearnJavascript/script.js
+++ LearnJavascript/script.js
@@ -1,6 +1,9 @@
-//製作一個基本的計算機
-var num1 = prompt("請輸入第1個數字");
-var num2 = prompt("請輸入第2個數字");
-num1 = parseFloat(num1);  //parseInt 把字串轉換成小數
-num2 = parseFloat(num2);
-document.write(num1 + num2);
+
+function add(num1,num2) {
+    document.write(num1 + num2);
+    document.write("<br/>")
+    return 10;
+    document.write("你好");
+}
+value = add(3,2);
+document.write( value );
